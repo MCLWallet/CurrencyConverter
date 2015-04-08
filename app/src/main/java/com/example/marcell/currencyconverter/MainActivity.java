2cc1f3bd99b803872b402c9777da21bf08289996
a1209638 <a1209638@unet.univie.ac.at>
2015-04-08 23:08:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/marcell/currencyconverter/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.marcell.currencyconverter;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickRechnen(View v){
+        Double eingabe, ausgabe;
+
+        EditText e1 = (EditText)findViewById(R.id.editText);
+        TextView t1 = (TextView)findViewById(R.id.textView);
+
+
+        eingabe= Double.parseDouble(e1.getText().toString());
+        ausgabe= eingabe*1000;
+
+        t1.setText(Double.toString(ausgabe));
+
+    }
+}
