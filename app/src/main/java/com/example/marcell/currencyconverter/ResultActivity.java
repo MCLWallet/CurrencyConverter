3810a045299f65ae20ab7e3e9fc995e0209052f7
a1209638 <a1209638@unet.univie.ac.at>
2015-04-09 19:35:20 +0200

    First Draft

--- app/src/main/java/com/example/marcell/currencyconverter/ResultActivity.java
+++ app/src/main/java/com/example/marcell/currencyconverter/ResultActivity.java
@@ -5,26 +5,31 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
 
-    public final static String EXTRA_MESSAGE="com.example.marcell.currencyconverter.MESSAGE";
+public class ResultActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_result);
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Read the textView
+        TextView t1 = (TextView)findViewById(R.id.textView3);
+        t1.setText(message);
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_result, menu);
         return true;
     }
 
@@ -41,22 +46,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-
-    public void sendMessage(View v){
-        Double input;
-        EditText e1 = (EditText)findViewById(R.id.amount);
-        input = Double.parseDouble(e1.getText().toString());
-        if (input<0){
-            String ausgabe="No negative variables allowed!!!";
-            TextView t1 = (TextView)findViewById(R.id.textView);
-            t1.setText(ausgabe);
-        }
-        Intent intent = new Intent(this, ResultActivity.class);
-        String message = e1.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-
     }
 }
